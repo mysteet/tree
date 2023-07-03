58025de8afd53651a5538a6e4e17076d29d938e5
v.yemelianov <v.yemelianov@voverc.com>
2023-07-03 18:07:36 +0300

    My street seo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Helmet} from 'react-helmet';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Map from './Map'; // Import the Map component
 
@@ -6,6 +7,11 @@ import Map from './Map'; // Import the Map component
 function App() {
     return (
         <Router>
+            <Helmet>
+                <title>MyStreet: Discover the History Behind Streets</title>
+                <meta name="description" content="MyStreet is an interactive map application that reveals the history and significance of street names in Ukraine. Explore the stories behind the names and uncover the cultural heritage of your city streets." />
+                <meta name="keywords" content="My Street, Street story, Street History, Ukraine, Map, Historical Significance, Cultural Heritage, Street Names, OpenAI, AI-Generated Stories" />
+            </Helmet>
             <Routes>
                 <Route path="/" element={<Map />}/> {/* Use the Map component */}
                 {
